0001e97dc20ebb1f9e44ee1b4b91dd6b4502e2c8
zzrjack <1220939105@qq.com>
2022-05-28 15:53:01 +0800

    开发自己的包

---
+++ 02.test.js
@@ -0,0 +1,3 @@
+const itheima = require('./itheima-tools/index');
+const dtStr = itheima.dateFormat(new Date());
+console.log(dtStr);
---
+++ itheima-tools/index.js
@@ -0,0 +1,19 @@
+//包的入口文件
+//格式化时间方法
+function dateFormat(dateStr) {
+	const dt = new Date(dateStr);
+	const y = dt.getFullYear();
+	const m = padZero(dt.getMonth() + 1);
+	const d = padZero(dt.getDay());
+	const hh = padZero(dt.getHours());
+	const mm = padZero(dt.getMinutes());
+	const ss = padZero(dt.getSeconds());
+	return `${y}-${m}-${d} ${hh}:${mm}:${ss}`;
+}
+//补零
+function padZero(n) {
+	return n > 9 ? n : '0' + n;
+}
+module.exports = {
+	dateFormat: dateFormat,
+};
